469289e3dda136e9c8660d27c88ad92803a95ef8
rafaelasantana <60327926+rafaelasantana@users.noreply.github.com>
2023-06-14 13:02:16 +0200

    connecting to MongoDB, adjusting API functions

--- src/app/models/db-schemas.model.ts
+++ src/app/models/db-schemas.model.ts
@@ -1,14 +1,11 @@
-import { mongo } from "mongoose";
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // mongoose schema:
 // define object with field names as keys and desired datatype as values
 // can nest other fields within a field as necessary
 
-
-// HighscoreSchema with username and score fields
+// HighscoreSchema with username, score and date fields
 const HighscoreSchema = new Schema({
     username: {
         type: String,
@@ -17,22 +14,49 @@ const HighscoreSchema = new Schema({
     score: {
         type: Number,
         required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
 });
 
 // HighscoreListSchema consists of a list of HighscoreSchemas
 const HighscoreListSchema = new Schema({
     highscores: [HighscoreSchema]
-})
+});
+
+// UserSchema with email, password, token and createdAt date
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String,
+        default: null
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+      }
+});
 
 // create mongoose models
-// use these models as interface to interact with the database
+// use these models as an interface to interact with the database
 const Highscore = mongoose.model('Highscore', HighscoreSchema);
 const HighscoreList = mongoose.model('HighscoreList', HighscoreListSchema);
+const User = mongoose.model('User', UserSchema);
 
 // export mongoose models for use in other parts of the app
 // can access models in other files that import them
 module.exports = {
     Highscore,
-    HighscoreList
-}
+    HighscoreList,
+    User
+};
--- src/app/server/server.js
+++ src/app/server/server.js
@@ -8,20 +8,27 @@ const { v4: uuidv4 } = require('uuid');
 // connect to mongoDB
 const mongoose = require('mongoose');
 // import mongoose models
-const { HighScore, HighScoreList } = require('../models/highscore-db-table.model');
-const { User } = require('../models/user-db-table.model');
+const { Highscore, HighscoreList, User } = require('../models/db-schemas.model.ts');
 
-// in-memory database of users with default user
-const usersDB = [
-    {
-        email: 'test@test.at',
-        password: '12345678',
-        token: uuidv4()
-    }
-];
+// start my-mongodb container directly in Docker dashboard
+// or start a new container with:
+// $ docker run --name my-mongodb-container -p 27017:27017 -d mongo
 
-// in-memory database of highscores
-const highscoresDB = [];
+// connection URL to MongoDB running on Docker
+const mongoURL = 'mongodb://localhost:27017/my-mongodb';
+
+// Connect to MongoDB
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Test connection to MongoDB
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
 
 // bodyParser parses the request body and makes it available as req.body object
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,93 +38,162 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
 
 // users POST Route
-app.post('/users', (req, res) => {
+// creates a new user with a token or returns an error if the user email already exists in the DB
+app.post('/users', async (req, res) => {
     // extract data from the post request using the req.body object
     // destructuring assignment:
     // takes the email and password (keys in post request) values and stores them in the email, password variables
     const { email, password } = req.body;
 
-    // check if email already exists in the in-memory database
-    if (usersDB.includes(email)) {
-        // return error message (400 status -> bad request)
-        return res.status(400).json({ error: 'email already exists'});
+    try {
+        const existingUser = await User.findOne({ email });
+        // check if user already exists
+        if (existingUser) {
+            // send error response
+            return res.status(400).json({ error: 'Email already in use' });
+        }
+        // generate token for this user
+        const token = uuidv4();
+        // create new user
+        const newUser = new User({ email, password, token });
+        // save user to the database
+        await newUser.save().exec();
+        // send success response
+        res.status(201).json({ message: 'User registered successfully', token })
     }
-
-    // generate token for this user
-    const token = uuidv4();
-
-    // add email, password and token to the in-memory database
-    usersDB.push({ email, password, token });
-
-    // send success response to client with the authentification token (200 status -> OK)
-    res.status(201).json({ message: 'User registered successfully', token });
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
-// Sessions DELETE route
-app.delete('/sessions', (req, res) => {
-    // get token from the request headers
-    const token = req.headers.authorization?.split(' ')[1];
+// Sessions POST routes
+// logs user in by setting a new access token or returns an error if the user credentials are invalid
+app.post('/sessions', async (req, res) => {
+    // get user credentials from request body
+    const { email, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ email, password });
 
-    console.log('token: ' + token);
-    // check if token exists
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized'});
+        // check if login credentials are valid
+        if (existingUser) {
+            // set token for user if not set yet
+            if (!existingUser.token) {
+                // generate new token
+                const newToken = uuidv4();
+                // set new token for this user
+                existingUser.token = newToken;
+                // save to DB
+                existingUser.save();
+            }
+            // send success response
+            res.status(200).json({ message: 'Login successful', username: existingUser.email, token: existingUser.token });
+        }
+        // send error response
+        else {
+            res.status(401).json({ message: 'Invalid credentials' })
+        }
     }
-    // get index for the user with this token in the in-memory db
-    const userIndex = usersDB.findIndex((user) => user.token === token);
-
-    // check if the index is valid
-    if (userIndex !== -1) {
-        // set user's token to null
-        usersDB[userIndex].token = null;
-        // return success response
-        return res.status(200).json({ message: 'Logout successful.'});
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal server error' });
     }
-
-    // send error response if token is invalid
-    return res.status(401).json({ message: 'Invalid token' });
-
 })
 
-// Sessions POST routes
-app.post('/sessions', (req, res) => {
-    const { email, password } = req.body;
+// Sessions DELETE route
+// logs user out by deleting the access token or returns an error if the token does not exist or is invalid
+app.delete('/sessions', async (req, res) => {
+    // get token from the request headers with the authorization key
+    const token = req.headers.authorization?.split(' ')[1];
 
-    // check if credentials are valid
-    const user = usersDB.find(u => u.email === email && u.password === password);
-    // set this user's token in the in-memory db and send success message
-    if(user) {
-        // check if user has no token yet
-        if (!user.token) {
-            // generate new token
-            const newToken = uuidv4();
-            // set new token for this user
-            user.token = newToken;
+    try {
+        // check if a token was sent in the header
+        if (!token) {
+            return res.status(401).json({ message: 'Unauthorized' });
         }
-        // send success response
-        res.status(200).json({ message: 'Login successful', username: user.email, token: user.token});
+        // find the user with this token
+        const user = await User.findOne({ token });
+
+        // check if user with this token exists in the DB
+        if (user) {
+            // set user's token to null
+            user.token = null;
+            // sav changes to user
+            await user.save();
+            // return success response
+            return res.status(200).json({ message: 'Logout successful.' });
+        }
+        // send error response if token is invalid
+        return res.status(401).json({ message: 'Invalid token' });
+
     }
-    // send error response
-    else {
-        res.status(401).json({ message: 'Invalid credentials.' })
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal server error' });
     }
+
 })
 
 // Highscores POST route
-app.post('/highscores', (req, res) => {
+// updates the highscore for this user in the HighScoreList
+// or adds a new highscore entry to the HighScoreList if there is no entry with this username yet
+app.post('/highscores', async (req, res) => {
     const { username, score } = req.body;
-    // add highscore and username to the database
-    highscoresDB.push({ username, score });
-    // send success status code and success message
-    res.status(201).json({ message: 'Highscore added successfully' });
+
+    try {
+        // Find the existing highscore list or create a new one if it doesn't exist
+        let highscoreList = await HighscoreList.findOne();
+
+        if (!highscoreList) {
+            highscoreList = new HighscoreList();
+        }
+
+        // search for this username in the highscores list
+        const existingUserInList = highscoreList.highscores.find(
+            (highscore) => highscore.username === username
+        );
+
+        // check if username already exists in the highscores list
+        if (existingUserInList) {
+            // update highscore for this username with new score
+            existingUserInList.score = score;
+        }
+        // create new highscore entry for this user
+        else {
+            // create new highscore entry with username and score
+            const newHighscore = new Highscore({ username, score });
+            // push new entry to the list
+            highscoreList.highscores.push(newHighscore);
+        }
+
+        // save new highscore
+        await highscoreList.save();
+        // send success status code and success message
+        res.status(201).json({ message: 'Highscore added successfully' });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 })
 
 // Highscores GET route
-app.get('/highscores', (req, res) => {
-    // send highscoresDB array as json object
-    res.status(200).json(highscoresDB);
+// returns all unique highscores from HighscoreList in the database
+app.get('/highscores', async (req, res) => {
+    try {
+        // retrieve HighscoreList from the database
+        const highscoreList = await HighscoreList.findOne();
+        // extract highscores array from the list
+        const highscores = highscoreList.highscores;
+        // send highscores array as json object
+        res.status(200).json(highscores);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 })
